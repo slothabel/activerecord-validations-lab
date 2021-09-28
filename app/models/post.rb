700c65cfae71aeddb92d15b17557cbf36f720dc2
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-09-28 16:29:59 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", 'Non-Fiction']  }
+
+    include ActiveModel::Validations
+    validates_with TitleValidator
+
 end
