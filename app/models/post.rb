76a70dc60d2b69427272c22eec3f19db9f45175b
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-09-28 16:45:11 -0400

    commit tree 9eaa1bad036251ef660d5a5a517f80c035d8302a

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,15 @@ class Post < ActiveRecord::Base
     validates :title, presence: true
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
-    validates :category, inclusion: { in: ["Fiction", 'Non-Fiction']  }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)  }
+    validate :validator?
+    # include ActiveModel::Validations
+    # validates_with TitleValidator
 
-    include ActiveModel::Validations
-    validates_with TitleValidator
+    def validator?
+        unless !title.nil? && title.match?(/Won't Believe/ || /Secret/ || /Top \d+/ || /Guess/)
+            errors[:title] << "Need clickbait"
+        end
+end
 
 end
